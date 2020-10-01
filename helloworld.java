5943bd75876dc02ab7f4e2acc2c0d0f54fdd0612
VritikaMalhotra <67013367+VritikaMalhotra@users.noreply.github.com>
2020-10-02 01:27:36 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,9 @@
-public class helloWorld{
-public static void main(String args[]){
-System.out.print("hello world");
-}
+//Aim: To print Hello world using java.
+
+public class helloWorld
+{
+  public static void main(String args[])
+  {
+    System.out.print("hello world");
+  }
 }
